b8e7cf6f5f4627433b430cfabd3753296ac916b3
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2026-01-20 11:43:34 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center text-3xl font-bold">
+      Tailwind v3 is working 🚀
+    </div>
+  );
+}
+
+export default App;
