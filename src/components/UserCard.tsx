61fe6ab167d10dd82e68a6f543b8291fbbba7971
Pratit Bangdiwala <pratit.bangdiwala@neosoftmail.com>
2026-01-27 11:52:20 +0530

    fix: user card conditional rendering

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,19 +1,20 @@
 import { FaCaretDown, FaCaretUp } from "react-icons/fa"
 import type { UserCardProps } from "../utils/types"
+import { memo } from "react"
 
 const UserCard: React.FC<UserCardProps> = ({ rank, name, networth, place }) => {
     return (
         <div className="whitespace-nowrap bg-white rounded-lg px-4 py-3 flex-1 shadow-md">
             <div className="flex items-center gap-1">
-                <p>{rank}.</p>
-                <p className="font-bold capitalize text-lg">{name}</p>
+                {rank && <p>{rank}.</p>}
+                {name && <p className="font-bold capitalize text-lg">{name}</p>}
             </div>
             <div className="mt-1">
-                <p className="capitalize">net worth: {networth}</p>
-                <p className="flex items-center capitalize">position: {place === 'up' ? <FaCaretUp color="#66BB6A" size={28} /> : <FaCaretDown color="#F44336" size={28} />}</p>
+                {networth && <p className="capitalize">net worth: {networth}</p>}
+                {place && <p className="flex items-center capitalize">position: {place === 'up' ? <FaCaretUp color="#66BB6A" size={28} /> : <FaCaretDown color="#F44336" size={28} />}</p>}
             </div>
         </div>
     )
 }
 
-export default UserCard
+export default memo(UserCard)
